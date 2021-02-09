8c4f951f4e464db53ae3d32841551fda36d4af51
Peter <ptrubey@ucsc.edu>
2021-02-08 22:00:45 -0700

    simplex new priors

---
+++ test_simplex_30_lowinf.py
@@ -0,0 +1,24 @@
+from simplex import *
+from data import Data_From_Raw
+from pandas import read_csv
+
+path = './datasets/ivt_nov_mar.csv'
+
+if __name__ == '__main__':
+    raw = read_csv(path)
+    data = Data_From_Raw(raw, True)
+    data.write_empirical('./output/fmix/empirical.csv')
+
+    fmix = FMIX_Chain(
+            data, 30,
+            GammaPrior(0.1,0.1),
+            DirichletPrior(0.5),
+            )
+    fmix.sample(50000)
+    fmix.write_to_disk('./output/fmix/results_30_lowinf.db',25000,5)
+
+    res = FMIX_Result('./output/fmix/results_30_lowinf.db')
+    res.write_posterior_predictive('./output/fmix/postpred_30_lowinf.csv')
+
+
+# EOF
