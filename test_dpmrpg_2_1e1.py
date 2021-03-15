f48e5d9cbb47420f08fba20e26fcdd965b22b519
Peter <ptrubey@ucsc.edu>
2021-03-14 19:19:13 -0600

    restricted projected gamma model

---
+++ test_dpmrpg_2_1e1.py
@@ -0,0 +1,30 @@
+from dp_rprojgamma import DPMPG, ResultDPMPG
+from projgamma import GammaPrior
+from data import Data_From_Raw
+from pandas import read_csv
+from random import shuffle
+
+
+path = './datasets/ivt_nov_mar.csv'
+
+
+if __name__ == '__main__':
+    raw  = read_csv(path)
+    # cols = raw.columns.values.tolist()
+    # shuffle(cols)
+    # raw  = raw.reindex(columns = cols)
+    data = Data_From_Raw(raw, True)
+    data.write_empirical('./output/dpmrpg/empirical.csv')
+
+    dpmpg = DPMPG(
+        data,
+        prior_eta = GammaPrior(2.,1e1)
+        )
+    dpmpg.initialize_sampler(20000)
+    dpmpg.sample(20000)
+    dpmpg.write_to_disk('./output/dprmpg/results_2_1e1.db', 10000,2)
+
+    res = ResultDPMPG('./output/dprmpg/results_2_1e1.db')
+    res.write_posterior_predictive('./output/dpmrpg/postpred_2_1e1.csv')
+
+# EOF
