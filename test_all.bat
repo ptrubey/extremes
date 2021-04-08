d109bd65b917738888705023c1072014cf02bdbe
Peter <ptrubey@ucsc.edu>
2021-04-08 11:24:24 -0600

    xz

--- test_all.bat
+++ test_all.bat
@@ -1,68 +1,70 @@
 ECHO "doing everything"
 
-start /low cmd /k python test_dpd.py 50000 20000 30 2 1e1
-start /low cmd /k python test_dpd.py 50000 20000 30 2 1e0
-start /low cmd /k python test_dpd.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_dpd.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dpd.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dpd.py 50000 20000 30 2 1e-1
 
-start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e1
-start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e0
-start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e-1
 
-start /low cmd /k python test_dppg.py 50000 20000 30 2 1e1
-start /low cmd /k python test_dppg.py 50000 20000 30 2 1e0
-start /low cmd /k python test_dppg.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_dppg.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dppg.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dppg.py 50000 20000 30 2 1e-1
+ 
+REM start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e-1
 
-start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e1
-start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e0
-start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_md.py 50000 20000 30 10
+REM start /low cmd /k python test_md.py 50000 20000 30 20
+REM start /low cmd /k python test_md.py 50000 20000 30 30
 
-start /low cmd /k python test_md.py 50000 20000 30 10
-start /low cmd /k python test_md.py 50000 20000 30 20
-start /low cmd /k python test_md.py 50000 20000 30 30
+REM start /low cmd /k python test_mgd.py 50000 20000 30 10
+REM start /low cmd /k python test_mgd.py 50000 20000 30 20
+REM start /low cmd /k python test_mgd.py 50000 20000 30 30
 
-start /low cmd /k python test_mgd.py 50000 20000 30 10
-start /low cmd /k python test_mgd.py 50000 20000 30 20
-start /low cmd /k python test_mgd.py 50000 20000 30 30
+REM start /low cmd /k python test_mpg.py 50000 20000 30 10
+REM start /low cmd /k python test_mpg.py 50000 20000 30 20
+REM start /low cmd /k python test_mpg.py 50000 20000 30 30
 
-start /low cmd /k python test_mpg.py 50000 20000 30 10
-start /low cmd /k python test_mpg.py 50000 20000 30 20
-start /low cmd /k python test_mpg.py 50000 20000 30 30
+REM start /low cmd /k python test_mprg.py 50000 20000 30 10
+REM start /low cmd /k python test_mprg.py 50000 20000 30 20
+REM start /low cmd /k python test_mprg.py 50000 20000 30 30
 
-start /low cmd /k python test_mprg.py 50000 20000 30 10
-start /low cmd /k python test_mprg.py 50000 20000 30 20
-start /low cmd /k python test_mprg.py 50000 20000 30 30
+REM start /low cmd /k python test_dppgln.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dppgln.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dppgln.py 50000 20000 30 2 1e-1
 
-REM start /low cmd /k python test_dppgln.py 20000 10000 2 2 1e1
-REM start /low cmd /k python test_dppgln.py 20000 10000 2 2 1e0
-REM start /low cmd /k python test_dppgln.py 20000 10000 2 2 1e-1
+REM start /low cmd /k python test_dpprgln.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dpprgln.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dpprgln.py 50000 20000 30 2 1e-1
 
-REM start /low cmd /k python test_dpprgln.py 20000 10000 2 2 1e1
-REM start /low cmd /k python test_dpprgln.py 20000 10000 2 2 1e0
-REM start /low cmd /k python test_dpprgln.py 20000 10000 2 2 1e-1
+REM UP TO HERE DONE.
 
-REM start /low cmd /k python test_dpgdln.py 20000 10000 2 2 1e1
-REM start /low cmd /k python test_dpgdln.py 20000 10000 2 2 1e0
-REM start /low cmd /k python test_dpgdln.py 20000 10000 2 2 1e-1
+REM start /low cmd /k python test_dpgdln.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dpgdln.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dpgdln.py 50000 20000 30 2 1e-1
 
-REM start /low cmd /k python test_dpdln.py 20000 10000 2 2 1e1
-REM start /low cmd /k python test_dpdln.py 20000 10000 2 2 1e0
-REM start /low cmd /k python test_dpdln.py 20000 10000 2 2 1e-1
+REM start /low cmd /k python test_dpdln.py 50000 20000 30 2 1e1
+REM start /low cmd /k python test_dpdln.py 50000 20000 30 2 1e0
+REM start /low cmd /k python test_dpdln.py 50000 20000 30 2 1e-1
 
-REM start /low cmd /k python test_mpgln.py 20000 10000 2 10
-REM start /low cmd /k python test_mpgln.py 20000 10000 2 20
-REM start /low cmd /k python test_mpgln.py 20000 10000 2 30
+REM start /low cmd /k python test_mpgln.py 50000 20000 30 10
+REM start /low cmd /k python test_mpgln.py 50000 20000 30 20
+REM start /low cmd /k python test_mpgln.py 50000 20000 30 30
 
-REM start /low cmd /k python test_mprgln.py 20000 10000 2 10
-REM start /low cmd /k python test_mprgln.py 20000 10000 2 20
-REM start /low cmd /k python test_mprgln.py 20000 10000 2 30
+REM start /low cmd /k python test_mprgln.py 50000 20000 30 10
+REM start /low cmd /k python test_mprgln.py 50000 20000 30 20
+REM start /low cmd /k python test_mprgln.py 50000 20000 30 30
 
-REM start /low cmd /k python test_mgdln.py 20000 10000 2 10
-REM start /low cmd /k python test_mgdln.py 20000 10000 2 20
-REM start /low cmd /k python test_mgdln.py 20000 10000 2 30
+REM start /low cmd /k python test_mgdln.py 50000 20000 30 10
+REM start /low cmd /k python test_mgdln.py 50000 20000 30 20
+REM start /low cmd /k python test_mgdln.py 50000 20000 30 30
 
-REM start /low cmd /k python test_mdln.py 20000 10000 2 10
-REM start /low cmd /k python test_mdln.py 20000 10000 2 20
-REM start /low cmd /k python test_mdln.py 20000 10000 2 30
+REM start /low cmd /k python test_mdln.py 50000 20000 30 10
+REM start /low cmd /k python test_mdln.py 50000 20000 30 20
+REM start /low cmd /k python test_mdln.py 50000 20000 30 30
 
 REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e1
 REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e0
--- test_all.sh
+++ test_all.sh
@@ -1,69 +1,87 @@
 #!/bin/bash
 ECHO "doing everything"
 
-nohup python test_dpd.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dpd.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dpd.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_dpd.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup python test_dpd.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup python test_dpd.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-nohup python test_dpgd.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dpgd.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dpgd.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_dpgd.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup python test_dpgd.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup python test_dpgd.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-nohup python test_dppg.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dppg.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dppg.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_dppg.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup python test_dppg.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup python test_dppg.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-nohup python test_dpprg.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dpprg.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dpprg.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_dpprg.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup python test_dpprg.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup python test_dpprg.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-nohup python test_md.py 20000 10000 2 10 > /dev/null 2>&1 &
-nohup python test_md.py 20000 10000 2 20 > /dev/null 2>&1 &
-nohup python test_md.py 20000 10000 2 30 > /dev/null 2>&1 &
+# nohup python test_md.py 50000 20000 30 10 > /dev/null 2>&1 &
+# nohup python test_md.py 50000 20000 30 20 > /dev/null 2>&1 &
+# nohup python test_md.py 50000 20000 30 30 > /dev/null 2>&1 &
 
-nohup python test_mgd.py 20000 10000 2 10 > /dev/null 2>&1 &
-nohup python test_mgd.py 20000 10000 2 20 > /dev/null 2>&1 &
-nohup python test_mgd.py 20000 10000 2 30 > /dev/null 2>&1 &
+# nohup python test_mgd.py 50000 20000 30 10 > /dev/null 2>&1 &
+# nohup python test_mgd.py 50000 20000 30 20 > /dev/null 2>&1 &
+# nohup python test_mgd.py 50000 20000 30 30 > /dev/null 2>&1 &
 
-nohup python test_mpg.py 20000 10000 2 10 > /dev/null 2>&1 &
-nohup python test_mpg.py 20000 10000 2 20 > /dev/null 2>&1 &
-nohup python test_mpg.py 20000 10000 2 30 > /dev/null 2>&1 &
+# nohup python test_mpg.py 50000 20000 30 10 > /dev/null 2>&1 &
+# nohup python test_mpg.py 50000 20000 30 20 > /dev/null 2>&1 &
+# nohup python test_mpg.py 50000 20000 30 30 > /dev/null 2>&1 &
 
-nohup python test_mprg.py 20000 10000 2 10 > /dev/null 2>&1 &
-nohup python test_mprg.py 20000 10000 2 20 > /dev/null 2>&1 &
-nohup python test_mprg.py 20000 10000 2 30 > /dev/null 2>&1 &
+# nohup python test_mprg.py 50000 20000 30 10 > /dev/null 2>&1 &
+# nohup python test_mprg.py 50000 20000 30 20 > /dev/null 2>&1 &
+# nohup python test_mprg.py 50000 20000 30 30 > /dev/null 2>&1 &
 
-nohup python test_dppgln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dppgln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dppgln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_dppgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup python test_dppgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup python test_dppgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-nohup python test_dpprgln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dpprgln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dpprgln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_dpprgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup python test_dpprgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup python test_dpprgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-nohup python test_dpgdln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dpgdln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dpgdln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# UP TO HERE DONE
 
-nohup python test_dpdln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-nohup python test_dpdln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-nohup python test_dpdln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+nohup python test_dpgdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+nohup python test_dpgdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+nohup python test_dpgdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-# nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+nohup python test_dpdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+nohup python test_dpdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+nohup python test_dpdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
+
+nohup python test_mpgln.py 50000 20000 30 10 > /dev/null 2>&1 &
+nohup python test_mpgln.py 50000 20000 30 20 > /dev/null 2>&1 &
+nohup python test_mpgln.py 50000 20000 30 30 > /dev/null 2>&1 &
+
+nohup python test_mprgln.py 50000 20000 30 10 > /dev/null 2>&1 &
+nohup python test_mprgln.py 50000 20000 30 20 > /dev/null 2>&1 &
+nohup python test_mprgln.py 50000 20000 30 30 > /dev/null 2>&1 &
+
+nohup python test_mgdln.py 50000 20000 30 10 > /dev/null 2>&1 &
+nohup python test_mgdln.py 50000 20000 30 20 > /dev/null 2>&1 &
+nohup python test_mgdln.py 50000 20000 30 30 > /dev/null 2>&1 &
+
+nohup python test_mdln.py 50000 20000 30 10 > /dev/null 2>&1 &
+nohup python test_mdln.py 50000 20000 30 20 > /dev/null 2>&1 &
+nohup python test_mdln.py 50000 20000 30 30 > /dev/null 2>&1 &
+
+# nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 #
-# nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
-# nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 #
-# nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 20000 10000 2 2 1e1 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 20000 10000 2 2 1e0 > /dev/null 2>&1 &
-# nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 20000 10000 2 2 1e-1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
+# nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
 
 
 
