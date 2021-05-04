085db36421cfd5d5b398452b856a7fd604f9b8b4
Peter <ptrubey@ucsc.edu>
2021-05-04 14:18:04 -0600

    updated testing scripts

--- test_all.bat
+++ test_all.bat
@@ -1,107 +1,20 @@
-ECHO "doing everything"
+REM DOING EVERYTHING
 
-REM start /low cmd /k python test_dpd.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dpd.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dpd.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dpd 50000 20000 30 --eta_shape 2 --eta_rate 1e-1
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dpgd 50000 20000 30 --eta_shape 2 --eta_rate 1e-1
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dppg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dpprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1
 
-REM start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dpgd.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 md 50000 20000 30 --nMix 30
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 mgd 50000 20000 30 --nMix 30
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 mpg 50000 20000 30 --nMix 30
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 mprg 50000 20000 30 --nMix 30
 
-REM start /low cmd /k python test_dppg.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dppg.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dppg.py 50000 20000 30 2 1e-1
- 
-REM start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dpprg.py 50000 20000 30 2 1e-1
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vd 50000 20000 30
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vgd 50000 20000 30
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vpg 50000 20000 30
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vprg 50000 20000 30
 
-REM start /low cmd /k python test_md.py 50000 20000 30 10
-REM start /low cmd /k python test_md.py 50000 20000 30 20
-REM start /low cmd /k python test_md.py 50000 20000 30 30
-
-REM start /low cmd /k python test_mgd.py 50000 20000 30 10
-REM start /low cmd /k python test_mgd.py 50000 20000 30 20
-REM start /low cmd /k python test_mgd.py 50000 20000 30 30
-
-REM start /low cmd /k python test_mpg.py 50000 20000 30 10
-REM start /low cmd /k python test_mpg.py 50000 20000 30 20
-REM start /low cmd /k python test_mpg.py 50000 20000 30 30
-
-REM start /low cmd /k python test_mprg.py 50000 20000 30 10
-REM start /low cmd /k python test_mprg.py 50000 20000 30 20
-REM start /low cmd /k python test_mprg.py 50000 20000 30 30
-
-REM start /low cmd /k python test_dppgln.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dppgln.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dppgln.py 50000 20000 30 2 1e-1
-
-REM start /low cmd /k python test_dpprgln.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dpprgln.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dpprgln.py 50000 20000 30 2 1e-1
-
-REM UP TO HERE DONE.
-
-REM start /low cmd /k python test_dpgdln.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dpgdln.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dpgdln.py 50000 20000 30 2 1e-1
-
-REM start /low cmd /k python test_dpdln.py 50000 20000 30 2 1e1
-REM start /low cmd /k python test_dpdln.py 50000 20000 30 2 1e0
-REM start /low cmd /k python test_dpdln.py 50000 20000 30 2 1e-1
-
-REM start /low cmd /k python test_mpgln.py 50000 20000 30 10
-REM start /low cmd /k python test_mpgln.py 50000 20000 30 20
-REM start /low cmd /k python test_mpgln.py 50000 20000 30 30
-
-REM start /low cmd /k python test_mprgln.py 50000 20000 30 10
-REM start /low cmd /k python test_mprgln.py 50000 20000 30 20
-REM start /low cmd /k python test_mprgln.py 50000 20000 30 30
-
-REM start /low cmd /k python test_mgdln.py 50000 20000 30 10
-REM start /low cmd /k python test_mgdln.py 50000 20000 30 20
-REM start /low cmd /k python test_mgdln.py 50000 20000 30 30
-
-REM start /low cmd /k python test_mdln.py 50000 20000 30 10
-REM start /low cmd /k python test_mdln.py 50000 20000 30 20
-REM start /low cmd /k python test_mdln.py 50000 20000 30 30
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e1
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e0
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dppgln.py 20000 10000 2 2 1e-1
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpprgln.py 20000 10000 2 2 1e1
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpprgln.py 20000 10000 2 2 1e0
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpprgln.py 20000 10000 2 2 1e-1
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpgdln.py 20000 10000 2 2 1e1
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpgdln.py 20000 10000 2 2 1e0
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpgdln.py 20000 10000 2 2 1e-1
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpdln.py 20000 10000 2 2 1e1
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpdln.py 20000 10000 2 2 1e0
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_dpdln.py 20000 10000 2 2 1e-1
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mpgln.py 20000 10000 2 10
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mpgln.py 20000 10000 2 20
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mpgln.py 20000 10000 2 30
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mprgln.py 20000 10000 2 10
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mprgln.py 20000 10000 2 20
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mprgln.py 20000 10000 2 30
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mgdln.py 20000 10000 2 10
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mgdln.py 20000 10000 2 20
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mgdln.py 20000 10000 2 30
-
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mdln.py 20000 10000 2 10
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mdln.py 20000 10000 2 20
-REM start /low cmd /k mpiexec -np 5 python -m mpi4py test_mdln.py 20000 10000 2 30
-
-REM start /low cmd /k python test_dppn.py 20000 10000 2 2 1e1
-REM start /low cmd /k python test_dppn.py 20000 10000 2 2 1e0
-REM start /low cmd /k python test_dppn.py 20000 10000 2 2 1e-1
-
-
+REM start /low cmd /k python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dppn 50000 20000 30 --eta_shape 2 --eta_rate 1e-1
 
 REM EOF
--- test_all.sh
+++ test_all.sh
@@ -1,89 +1,20 @@
 #!/bin/bash
-ECHO "doing everything"
 
-# nohup python test_dpd.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dpd.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dpd.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-# nohup python test_dpgd.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dpgd.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dpgd.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-# nohup python test_dppg.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dppg.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dppg.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-# nohup python test_dpprg.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dpprg.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dpprg.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-# nohup python test_md.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_md.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_md.py 50000 20000 30 30 > /dev/null 2>&1 &
-
-# nohup python test_mgd.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mgd.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mgd.py 50000 20000 30 30 > /dev/null 2>&1 &
-
-# nohup python test_mpg.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mpg.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mpg.py 50000 20000 30 30 > /dev/null 2>&1 &
-
-# nohup python test_mprg.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mprg.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mprg.py 50000 20000 30 30 > /dev/null 2>&1 &
-
-# nohup python test_dppgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dppgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dppgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-# nohup python test_dpprgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dpprgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dpprgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-# UP TO HERE DONE
-
-# nohup python test_dpgdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dpgdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dpgdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dpd 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dpgd 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dppg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dpprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 #
-# nohup python test_dpdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-# nohup python test_dpdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-# nohup python test_dpdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 md 50000 20000 30 --nMix 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 mgd 50000 20000 30 --nMix 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 mpg 50000 20000 30 --nMix 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 mprg 50000 20000 30 --nMix 30 > /dev/null 2>&1 &
 #
-# nohup python test_mpgln.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mpgln.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mpgln.py 50000 20000 30 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vd 50000 20000 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vgd 50000 20000 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vpg 50000 20000 30 > /dev/null 2>&1 &
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 vprg 50000 20000 30 > /dev/null 2>&1 &
 #
-# nohup python test_mprgln.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mprgln.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mprgln.py 50000 20000 30 30 > /dev/null 2>&1 &
-#
-# nohup python test_mgdln.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mgdln.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mgdln.py 50000 20000 30 30 > /dev/null 2>&1 &
-#
-# nohup python test_mdln.py 50000 20000 30 10 > /dev/null 2>&1 &
-# nohup python test_mdln.py 50000 20000 30 20 > /dev/null 2>&1 &
-# nohup python test_mdln.py 50000 20000 30 30 > /dev/null 2>&1 &
-
-nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dppgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dpprgln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dpgdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 50000 20000 30 2 1e1 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 50000 20000 30 2 1e0 > /dev/null 2>&1 &
-nohup mpiexec -np 5 python -m mpi4py test_dpdln.py 50000 20000 30 2 1e-1 > /dev/null 2>&1 &
-
-
-
+# nohup python test_generic.py ./datasets/ivt_updated_nov_mar.csv ./output2 dppn 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 
 # EOF
