99dcd722a48d5faa004c4c34035b53a7440d3388
Peter <ptrubey@ucsc.edu>
2021-06-20 19:29:05 -0700

    sim scripts

--- sim_all.sh
+++ sim_all.sh
@@ -8,14 +8,15 @@ nohup python test_simulation.py ./simulated/sim_c6_m3 dphprg 50000 20000 30 --et
 nohup python test_simulation.py ./simulated/sim_c6_m6 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c6_m9 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c6_m12 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m3 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m6 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m9 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m12 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m3 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m6 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m9 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m12 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m3 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m6 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m9 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m12 dphprg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+
 
 nohup python test_simulation.py ./simulated/sim_c3_m3 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c3_m6 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
@@ -25,13 +26,13 @@ nohup python test_simulation.py ./simulated/sim_c6_m3 dphpg 50000 20000 30 --eta
 nohup python test_simulation.py ./simulated/sim_c6_m6 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c6_m9 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c6_m12 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m3 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m6 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m9 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
-nohup python test_simulation.py ./simulated/sim_c9_m12 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m3 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m6 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m9 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 nohup python test_simulation.py ./simulated/sim_c12_m12 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m3 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m6 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m9 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
+nohup python test_simulation.py ./simulated/sim_c20_m12 dphpg 50000 20000 30 --eta_shape 2 --eta_rate 1e-1 > /dev/null 2>&1 &
 
 # EOF
