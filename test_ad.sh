dd4ae5337f42f054c36011e170cedb5737474487
Peter <ptrubey@ucsc.edu>
2021-07-06 18:21:00 -0700

    updated anomaly detection script with lower eta rates

--- test_ad.sh
+++ test_ad.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nohup python test_generic.py ./datasets/ad_cardio_x.csv ./ad/cardio dphprg 50000 20000 30 --decluster False > /dev/null 2>&1 &
+nohup python test_generic.py ./datasets/ad_cardio_x.csv ./ad/cardio dphprg 50000 20000 30 --eta_rate 1e0 --decluster False > /dev/null 2>&1 &
 nohup python test_generic.py ./datasets/ad_cover_x.csv ./ad/cover mhprg 50000 20000 30 --nMix 50 --decluster False > /dev/null 2>&1 &
-nohup python test_generic.py ./datasets/ad_mammography_x.csv ./ad/mammography dphprg 50000 20000 30 --decluster False > /dev/null 2>&1 &
-nohup python test_generic.py ./datasets/ad_pima_x.csv ./ad/pima dphprg 50000 20000 30 --decluster False > /dev/null 2>&1 &
-nohup python test_generic.py ./datasets/ad_satellite_x.csv ./ad/satellite dphprg 50000 20000 30 --decluster False --quantile 0.97 > /dev/null 2>&1 &
+nohup python test_generic.py ./datasets/ad_mammography_x.csv ./ad/mammography dphprg 50000 20000 30 --eta_rate 1e0 --decluster False > /dev/null 2>&1 &
+nohup python test_generic.py ./datasets/ad_pima_x.csv ./ad/pima dphprg 50000 20000 30 --eta_rate 1e0 --decluster False > /dev/null 2>&1 &
+nohup python test_generic.py ./datasets/ad_satellite_x.csv ./ad/satellite dphprg 50000 20000 30 --eta_rate 1e0 --decluster False --quantile 0.97 > /dev/null 2>&1 &
 
 # EOF
