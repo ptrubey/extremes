9a9470515719da347ade4b7d5e96a42434322217
Peter <ptrubey@ucsc.edu>
2021-11-13 17:19:15 -0800

    varying p redux redux

--- sim_varying_p.sh
+++ sim_varying_p.sh
@@ -9,14 +9,14 @@ nohup python test_varying_p.py ./varying_p/sim_1_3 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_1_3 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_1_3 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_1_3 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 5  > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_1_3 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_2_4 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_4 dpppg 2 > /dev/null 2>&1 &
@@ -27,14 +27,14 @@ nohup python test_varying_p.py ./varying_p/sim_2_4 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_4 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_4 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_4 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_4 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_2_5 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_5 dpppg 2 > /dev/null 2>&1 &
@@ -45,14 +45,14 @@ nohup python test_varying_p.py ./varying_p/sim_2_5 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_5 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_5 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_5 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_5 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_2_6 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_6 dpppg 2 > /dev/null 2>&1 &
@@ -63,14 +63,14 @@ nohup python test_varying_p.py ./varying_p/sim_2_6 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_6 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_2_6 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_2_6 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_2_6 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_3_5 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_5 dpppg 2 > /dev/null 2>&1 &
@@ -81,14 +81,14 @@ nohup python test_varying_p.py ./varying_p/sim_3_5 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_5 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_5 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_5 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_5 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_3_6 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_6 dpppg 2 > /dev/null 2>&1 &
@@ -99,14 +99,14 @@ nohup python test_varying_p.py ./varying_p/sim_3_6 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_6 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_6 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_6 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_6 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_3_7 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_7 dpppg 2 > /dev/null 2>&1 &
@@ -117,14 +117,14 @@ nohup python test_varying_p.py ./varying_p/sim_3_7 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_7 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_7 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_7 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_7 dppprg 8 > /dev/null 2>&1 &
 
 nohup python test_varying_p.py ./varying_p/sim_3_8 dpppg 1 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_8 dpppg 2 > /dev/null 2>&1 &
@@ -135,13 +135,13 @@ nohup python test_varying_p.py ./varying_p/sim_3_8 dpppg 6 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_8 dpppg 7 > /dev/null 2>&1 &
 nohup python test_varying_p.py ./varying_p/sim_3_8 dpppg 8 > /dev/null 2>&1 &
 
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 1 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 2 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 3 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 4 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 5 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 6 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 7 > /dev/null 2>&1 &
-nohup python test_varying_p.py ./varying_p/sim_3_8 dpprpg 8 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 1 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 2 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 3 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 4 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 5 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 6 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 7 > /dev/null 2>&1 &
+nohup python test_varying_p.py ./varying_p/sim_3_8 dppprg 8 > /dev/null 2>&1 &
 
 # EOF
