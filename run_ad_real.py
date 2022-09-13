fdd38871ad9ba7986d411259bee66286d6818306
Peter <ptrubey@ucsc.edu>
2022-09-12 22:33:23 -0700

    solarflare run (non-xv)

--- run_ad_real.py
+++ run_ad_real.py
@@ -13,6 +13,7 @@ cardio = {
     'quantile'  : '0.95',
     'cats'      : '[15,16,17,18,19,20,21,22,23,24]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',    
     }
 cover = {
     'source'    : './ad/cover/data.csv',
@@ -21,6 +22,7 @@ cover = {
     'quantile'  : '0.998',
     'cats'      : '[9,10,11,12]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',
     }
 mammography = {
     'source'    : './ad/mammography/data.csv',
@@ -29,6 +31,7 @@ mammography = {
     'quantile'  : '0.95',
     'cats'      : '[5,6,7,8]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',
     }
 pima = {
     'source'    : './ad/pima/data.csv',
@@ -37,6 +40,7 @@ pima = {
     'quantile'  : '0.90',
     'cats'      : '[7,8,9,10,11,12]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',
     }
 satellite = {
     'source'    : './ad/satellite/data.csv',
@@ -45,8 +49,8 @@ satellite = {
     'quantile'  : '0.95',
     'cats'      : '[36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',
     }
-##
 annthyroid = {
     'source'    : './ad/annthyroid/data.csv',
     'outcome'   : './ad/annthyroid/outcome.csv',
@@ -54,6 +58,7 @@ annthyroid = {
     'quantile'  : '0.95',
     'cats'      : '[6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',
     }
 yeast = {
     'source'    : './ad/yeast/data.csv',
@@ -62,10 +67,22 @@ yeast = {
     'quantile'  : '0.90',
     'cats'      : '[4,5]',
     'decluster' : 'False',
+    'model'     : 'mdppprgln',
+    }
+## Categorical
+solarflare = {
+    'source'    : './ad/solarflare/data.csv',
+    'outcome'   : './ad/solarflare/outcome.csv',
+    'results'   : './ad/solarflare/results_{}_{}_{}.pkl',
+    'quantile'  : '0.90',
+    'cats'      : 'None',
+    'decluster' : 'False',
+    'model'     : 'cdppprgln',
     }
 
+
 # datasets = [cardio, cover, mammography, pima, satellite, annthyroid, yeast]
-datasets = [yeast]
+datasets = [solarflare]
 eta_alphas = ['2']
 eta_betas = ['1e-1','1e0','1e1','1e2']
 stepping = '1.03'
@@ -76,25 +93,24 @@ if __name__ == '__main__':
     process_args = []
 
     for dataset in datasets:
-        for model in models:
-            for ealpha in eta_alphas:
-                for ebeta in eta_betas:
-                    args = [
-                        sys.executable, 
-                        'test_generic.py', 
-                        dataset['source'],
-                        dataset['results'].format(model, ealpha, ebeta),
-                        model,
-                        '--outcome', dataset['outcome'],
-                        '--cats', dataset['cats'],
-                        '--quantile', dataset['quantile'],
-                        '--nSamp', '50000', '--nKeep', '30000', '--nThin', '10',
-                        '--eta_shape', ealpha, '--eta_rate', ebeta,
-                        '--decluster', dataset['decluster'],
-                        '--ntemps', ntemps, '--stepping', stepping,
-                        ]
-                    process_args.append(args)
-                    processes.append(Popen(args))
+        for ealpha in eta_alphas:
+            for ebeta in eta_betas:
+                args = [
+                    sys.executable, 
+                    'test_generic.py', 
+                    dataset['source'],
+                    dataset['results'].format(dataset['model'], ealpha, ebeta),
+                    dataset['model'],
+                    '--outcome', dataset['outcome'],
+                    '--cats', dataset['cats'],
+                    '--quantile', dataset['quantile'],
+                    '--nSamp', '50000', '--nKeep', '30000', '--nThin', '10',
+                    '--eta_shape', ealpha, '--eta_rate', ebeta,
+                    '--decluster', dataset['decluster'],
+                    '--ntemps', ntemps, '--stepping', stepping,
+                    ]
+                process_args.append(args)
+                processes.append(Popen(args))
 
     for process in processes:
         process.wait()
