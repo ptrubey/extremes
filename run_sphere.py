6bd2e79cc986a4880a16cb8fa76da68e70859918
Peter <ptrubey@ucsc.edu>
2023-05-08 13:23:24 -0700

    run_sphere.py

--- run_sphere.py
+++ run_sphere.py
@@ -15,7 +15,7 @@ from models import RealChains as Chains, RealResults as Results
 source_path = './simulated/sphere2/data_m*_r*_i*.csv'
 models      = ['sdpppg', 'sdppprg', 'sdpppgln', 'sdppprgln']
 # models      = ['sd']
-out_sql     = './simulated/sphere2/result.sql'
+out_sql     = './simulated/sphere2/result_highp.sql'
 out_table   = 'energy'
 
 def run_model_from_path_wrapper(args):
@@ -33,9 +33,10 @@ def run_model_from_path(path, modeltype):
     model = Chains[modeltype](
         data, 
         prior_eta = (2., 1.), 
-        p = 10,
+        p = 20,
         # p = 1,
         max_clust_count = 150,
+        prior_Sigma = (5, 1.),
         )
     
     model.sample(50000)
