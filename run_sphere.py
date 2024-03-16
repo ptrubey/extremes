e1f1b892109344434a66d071af35e5e63977cce1
Peter Trubey <peter@trubey.org>
2024-03-15 19:48:10 -0700

    fewer temps

--- run_sphere.py
+++ run_sphere.py
@@ -14,7 +14,7 @@ from models import RealChains as Chains, RealResults as Results
 
 source_path = './simulated/sphere2/data_m*_r*_i*.csv'
 models      = ['spypg'] # , 'sdpppg', 'sdppprg']
-out_sql     = './simulated/sphere2/result_240314.sql'
+out_sql     = './simulated/sphere2/result_240315.sql'
 out_table   = 'energy'
 
 def run_model_from_path_wrapper(args):
@@ -31,7 +31,8 @@ def run_model_from_path(path, modeltype):
     
     model = Chains[modeltype](
         data, 
-        prior_eta = (2., 1.), 
+        prior_eta = (2., 1.),
+        ntemps = 3,
         p = 10,
         max_clust_count = 150,
         )
