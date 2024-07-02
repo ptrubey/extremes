7f582d519f0bfb634f92071a03e106fe7e6c0349
Peter Trubey <peter@trubey.org>
2024-07-02 15:56:14 -0700

    reg

--- test_reg.py
+++ test_reg.py
@@ -66,16 +66,16 @@ if __name__ == '__main__':
             location    = x_location,
             interaction = x_interaction,
             )
-    model = Chain(data, p = 10)
-    model.sample(int(args.nSamp))
+    # model = Chain(data, p = 10)
+    # model.sample(int(args.nSamp))
     fname = 'sloshltd_{}_{}_{}'
     fname_pkl = fname + '.pkl'
     fpath_pkl = os.path.join('./test', fname_pkl)
-    model.write_to_disk(fpath_pkl, int(args.nKeep), int(args.nThin))
+    # model.write_to_disk(fpath_pkl, int(args.nKeep), int(args.nThin))
     fpath_del = fname + '_delta.csv'
     fpath_gam = fname + '_gamma.pkl'
       
-    res = Result('./test/results.pkl')
+    res = Result(fpath_pkl)
     postdeltas = res.samples.delta
     postalphas = res.generate_conditional_posterior_predictive_gammas()
 
