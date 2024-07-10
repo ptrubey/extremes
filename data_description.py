a77e419daf576a20a18537688da07e18445decf7
Peter Trubey <peter@trubey.org>
2024-07-09 19:22:40 -0700

    data description

--- data_description.py
+++ data_description.py
@@ -1,14 +1,15 @@
 import numpy as  np
 import pandas as pd
 import os
+import glob
 
 from data import RealData
-
+from collections import namedtuple
 
 path_full = './datasets/slosh/filtered_data.csv.gz'
 path_t90  = './datasets/slosh/slosh_thr.90.csv.gz'
 
-if __name__ == '__main__':
+if False: #__name__ == '__main__':
     slosh = pd.read_csv(path_full, compression = 'gzip')
     slosh_t90 = pd.read_csv(path_t90, compression='gzip').values
     ltd = ~slosh.MTFCC.isin(['C3061','C3081'])
@@ -32,8 +33,36 @@ if __name__ == '__main__':
     
     Is = np.array(Is).T
     desc = pd.DataFrame({'data' : names, 'N' : Ns, 'S' : Ss})
-    desc.to_csv('./test/threshold_summary.csv', index = False)
-    pd.DataFrame(Is).to_csv('./test/threshold_inclusion.csv', index = False)
+    # desc.to_csv('./test/threshold_summary.csv', index = False)
+    # pd.DataFrame(Is).to_csv('./test/threshold_inclusion.csv', index = False)
+
+path_wildcard = './test/sloshltd_*_delta.csv'
+ParmSet = namedtuple('ParmSet', 'dataset conc disc ncluster')
+
+def extract_parms(path):
+    fnames = os.path.splitext(os.path.split(path)[1])[0].split('_')
+    data = fnames[1]
+    conc = fnames[2]
+    disc = fnames[3]
+    df = pd.read_csv(path)
+    
+    def nunique(series):
+        return np.unique(series).shape[0]
+    
+    n = np.array([nunique(df.iloc[i]) for i in range(df.shape[0])])
+    
+    return ParmSet(data, conc, disc, n.mean())
+
+if __name__ == '__main__':
+    paths = glob.glob(path_wildcard)
+    parms = []
+    for path in paths:
+        try:
+            parms.append(extract_parms(path))
+        except pd.errors.EmptyDataError:
+            print('Failed {}'.format(path))
+    df = pd.DataFrame(parms)
+    df.to_csv('./test/sloshltd_cluster_counts.csv', index = False)
 
 # EOF
     
