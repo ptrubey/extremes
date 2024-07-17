c4ae9bc20b17b8ecb8de2fd903456a2fbecf1997
Peter Trubey <peter@trubey.org>
2024-07-16 17:34:51 -0700

    testtest

--- testtest.py
+++ testtest.py
@@ -16,7 +16,7 @@ sql_paths = [
     # './simulated/sphere2/result_240404.sql', # mean = -2, -2
     # './simulated/sphere2/result_240417.sql', # mean = -3, -2
     # './simulated/sphere2/result_240418.sql', # mean = -4, -3
-    './simulated/sphere2/result_240419.sql', # mean = -4, -2
+    './simulated/sphere2/result_240710.sql',   # mean = -4, -2
     ]
 
 def get_table(path, table):
