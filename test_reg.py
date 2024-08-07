111d9bddb8fb4106a95c52f456ad0e063194070c
Peter Trubey <peter@trubey.org>
2024-08-06 22:54:39 -0700

    test reg

--- test_reg.py
+++ test_reg.py
@@ -1,5 +1,6 @@
 import argparse, os, pickle
 import pandas as pd, numpy as np
+from geopy.distance import geodesic
 
 from model_spypprgr import RegressionData, Chain, Result, summarize, scale
 from energy import limit_cpu
@@ -16,6 +17,7 @@ def argparser():
     p.add_argument('--nSamp', default = '30000')
     p.add_argument('--nKeep', default = '15000')
     p.add_argument('--nThin', default = '15')
+    p.add_argument('--verbose', default = False)
     # p.add_argument('--realtype', default = 'threshold')
     # p.add_argument('--quantile', default = 0.95)
     # p.add_argument('--decluster', default = 'True')
@@ -26,8 +28,28 @@ def argparser():
 
     return p.parse_args()
 
+class Heap(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        return
+
+def fake_argparser():
+    p = Heap(
+        src = 'apt',
+        conc = 0.01,
+        disc = 0.01,
+        nSamp = 30000,
+        nKeep = 15000,
+        nThin = 15,
+        verbose = True,
+        )
+    return p
+
+basepath = './datasets/slosh/slosh_{}_data.csv.gz'
+
 if __name__ == '__main__':
     args = argparser()
+    # args = fake_argparser()
     limit_cpu()
 
     # slosh  = pd.read_csv(
@@ -35,19 +57,8 @@ if __name__ == '__main__':
     #     compression = 'gzip',
     #     )
     sloshx = pd.read_csv('./datasets/slosh/slosh_params.csv')
+    slosh  = pd.read_csv(basepath.format(args.src), compression = 'gzip')
 
-    if args.src == 'ltd':
-        slosh = pd.read_csv(
-            './datasets/slosh/slosh_ltd_data.csv.gz',
-            compression = 'gzip',
-            )
-    elif args.src == 'apt':
-        slosh = pd.read_csv(
-            './datasets/slosh/slosh_apt_data.csv.gz',
-            compression = 'gzip',
-            )
-    else:
-        raise
     sloshltd_ids = slosh.iloc[:,:8]
     sloshltd_obs = slosh.iloc[:,8:].T.values.astype(np.float64)
 
@@ -61,14 +72,22 @@ if __name__ == '__main__':
         locatx_par = summarize(sloshltd_ids[['long','lat']].values)
         locatx_std = scale(sloshltd_ids[['long','lat']].values, locatx_par)
     
-    sloshx_par.mean[-1] = locatx_par.mean[-1] # latitude values will be 
-    sloshx_par.sd[-1]   = locatx_par.sd[-1]   # on same scale for both datasets
+    # sloshx_par.mean[-1] = locatx_par.mean[-1] # latitude values will be 
+    # sloshx_par.sd[-1]   = locatx_par.sd[-1]   # on same scale for both datasets
     sloshx_std = scale(sloshx.values, sloshx_par)
         
     x_observation = sloshx_std
     x_location    = locatx_std
-    x_interaction = (sloshx_std[:,-1][None] * locatx_std[:,-1][:,None])[:,:,None]
 
+    storm_locs = list(map(tuple, sloshx[['lat','long']].values))
+    slosh_locs = list(map(tuple, sloshltd_ids[['lat','long']].values))
+    distances  = np.array([
+        [geodesic(slosh_loc, storm_loc).miles for slosh_loc in slosh_locs] 
+        for storm_loc in storm_locs
+        ])
+    # storms on the horizontal, locations on the vertical.
+    x_interaction = distances[:,:,None] / 100 # in decamiles.
+    # x_interaction = (sloshx_std[:,-1][None] * locatx_std[:,-1][:,None])[:,:,None]
     data = RegressionData(
             raw_real    = sloshltd_obs, 
             real_type   = 'threshold',
@@ -84,7 +103,7 @@ if __name__ == '__main__':
         concentration = float(args.conc), 
         discount = float(args.disc),
         )
-    model.sample(int(args.nSamp))
+    model.sample(int(args.nSamp), verbose = args.verbose)
     fname = 'sloshltd_{}_{}_{}'.format(args.src, args.conc, args.disc)
     fname_pkl = fname + '.pkl'
     fpath_pkl = os.path.join('./test', fname_pkl)
