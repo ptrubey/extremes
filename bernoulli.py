e0d4bff6bdba49f381bae34d415a9535aadcc108
Peter Trubey <peter@trubey.org>
2024-08-31 19:29:42 -0700

    bernoulli

--- bernoulli.py
+++ bernoulli.py
@@ -24,3 +24,41 @@ def bernoulli(n: float):
 
 seq1 = np.array([float(bernoulli(n)) for n in range(41)])[1:]
 seq2 = seq1[1::2]
+ints = np.arange(1, seq2.shape[0] + 1)
+
+def lgamma(x : np.ndarray, precision = 2):
+    out = np.empty(x.shape)
+    out += 0.5 * np.log(2 * np.pi)
+    out += (x - 0.5) * np.log(x)
+    out -= x
+    out += (
+        seq2[:precision] / 
+            (2 * ints[:precision]) /
+                (2 * ints[:precision] - 1) / 
+                    (x ** (2 * ints[:precision] - 1))
+        ).sum(axis = -1)
+
+if __name__ == '__main__':
+    from scipy.special import loggamma
+    
+    def f(x, precision = 5):
+        s = np.arange(1,seq2.shape[0] + 1)
+        out = 0.
+        out += 0.5 * np.log(2 * np.pi)
+        out += (x - 0.5) * np.log(x)
+        out -= x
+        out += (
+            seq2[:precision] / 
+                (2 * s[:precision] * (2 * s[:precision] - 1)) / 
+                    (x**(2 * s[:precision] - 1))
+            ).sum()
+        return out
+
+
+
+
+
+
+
+
+
