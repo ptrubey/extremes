dcc5009f28576f3a3e43b101078b71584d5f017a
Peter Trubey <peter@trubey.org>
2024-09-10 17:06:40 -0700

    vb explore

--- mc_vb_explore.py
+++ mc_vb_explore.py
@@ -9,10 +9,11 @@ from samplers import BaseSampler
 class Samples(object):
     def __init__(self, ns):
         self.alpha = np.empty(ns + 1)
+        self.beta  = np.empty(ns + 1)
         return
     pass
 
-class UnivariateGammaGammaMC(BaseSampler):
+class UnivariateResGammaGammaMC(BaseSampler):
     @property
     def curr_alpha(self):
         return self.samples.alpha[self.curr_iter]
@@ -23,7 +24,7 @@ class UnivariateGammaGammaMC(BaseSampler):
         self.curr_iter = 0
         return
 
-    def log_posterior_alpha(self, sigma):
+    def log_posterior_logalpha(self, sigma):
         return (
             + (np.exp(sigma) - 1) * self.lYs 
             - self.n * gammaln(np.exp(sigma))
@@ -34,8 +35,8 @@ class UnivariateGammaGammaMC(BaseSampler):
     def sample_alpha(self, curr_alpha):
         curr_log_alpha = np.log(curr_alpha)
         cand_log_alpha = normal(curr_log_alpha, scale = 5e-2)
-        curr_lp = self.log_posterior_alpha(curr_log_alpha)
-        cand_lp = self.log_posterior_alpha(cand_log_alpha)
+        curr_lp = self.log_posterior_logalpha(curr_log_alpha)
+        cand_lp = self.log_posterior_logalpha(cand_log_alpha)
         if np.log(uniform(1)) < cand_lp - curr_lp:
             return np.exp(cand_log_alpha)
         else:
@@ -56,6 +57,43 @@ class UnivariateGammaGammaMC(BaseSampler):
         self.b = b
         return
     pass
+
+class UnivariateGammaGammaMC(UnivariateResGammaGammaMC):
+    @property
+    def curr_beta(self):
+        return self.samples.beta[self.curr_iter]
+    def initialize_sampler(self, ns):
+        self.samples = Samples(ns)
+        self.samples.alpha[0] = 1.
+        self.samples.beta[0] = 1.
+        self.curr_iter = 0
+        return
+    def log_posterior_logalpha(self, sigma):
+        return (
+            + (np.exp(sigma) - 1) * self.lYs
+            - self.n * gammaln(np.exp(sigma))
+            + self.a * sigma
+            - self.b * sigma
+            + gammaln(self.n * np.exp(sigma) + self.c)
+            - (self.n * np.exp(sigma) + self.c) * (self.Ys + self.d)
+            )    
+    def sample_beta(self, alpha):
+        return gamma(
+            shape = self.n * alpha + self.c, 
+            scale = 1 / (self.Ys + self.d)
+            )
+    def iter_sample(self):
+        alpha = self.curr_alpha
+        self.curr_iter += 1
+        self.samples.alpha[self.curr_iter] = self.sample_alpha(alpha)
+        self.samples.beta[self.curr_iter]  = self.sample_beta(self.curr_alpha)
+        return
+    def __init__(self, Y, a, b, c, d):
+        super().__init__(Y, a, b)
+        self.Ys = Y.sum()
+        self.c  = c
+        self.d  = d
+        return
 
 class Adam(object):
     """ Adam optimizer """
@@ -112,7 +150,7 @@ class Adam(object):
         self.initialization(rate, decay1, decay2)
         return
 
-class UnivariateGammaGammaVB(object):
+class UnivariateResGammaGammaVB(object):
     Y   : np.ndarray
     n   : int
     lYs : np.ndarray
@@ -132,7 +170,7 @@ class UnivariateGammaGammaVB(object):
             - self.b * alpha
             )
         dtau = dmu * ete
-        return np.array((dmu.mean(), dtau.mean()))
+        return np.array((dmu.mean(axis = -1), dtau.mean(axis = -1)))
 
     def __init__(self, Y, a, b, ns = 20):
         self.Y = Y
@@ -144,19 +182,50 @@ class UnivariateGammaGammaVB(object):
         return
     pass
 
+class UnivariateGammaGammaVB(UnivariateResGammaGammaVB):
+    def gradient(self, mu, tau):
+        epsilon = normal(size = self.ns)
+        ete = np.exp(tau) * epsilon
+        alpha = np.exp(mu + ete)
+        dmu = (
+            + alpha * self.lYs
+            - self.n * digamma(alpha) * alpha
+            + (self.a - 1)
+            - self.b * alpha
+            + digamma(self.n * alpha + self.c)
+            - (self.n * alpha + self.c) * np.log(self.Ys + self.d)
+            )
+        dtau = dmu * ete
+        return np.array((dmu.mean(axis = -1), dtau.mean(axis = -1)))
+    
+    def __init__(self, Y, a, b, c, d, ns = 20):
+        super().__init__(Y, a, b, ns)
+        self.Ys = Y.sum()
+        self.c = c
+        self.d = d
+    pass
+
 if __name__ == '__main__':
     Y = gamma(shape = 5, scale = 1, size = 10)
-    a = 1
-    b = 1
+    abcd = (1,1,1,1)
     
-    mc = UnivariateGammaGammaMC(Y, a, b)
-    mc.sample(5000)
-    vb = UnivariateGammaGammaVB(Y, a, b)
+    # mc = UnivariateResGammaGammaMC(Y, a, b)
+    # mc.sample(5000)
+    # vb = UnivariateResGammaGammaVB(Y, a, b)
+
+    mc = UnivariateGammaGammaMC(Y, *abcd)
+    vb = UnivariateGammaGammaVB(Y, *abcd)
 
     def dloss(theta):
         return -vb.gradient(*theta)
     
-    adam = Adam(np.array((0.,0.)), dloss, rate = 1e-2, decay1 = 0.9, decay2 = 0.999)
+    adam = Adam(
+        np.array((0.,0.)), 
+        dloss, 
+        rate = 1e-2, 
+        decay1 = 0.9, 
+        decay2 = 0.999,
+        )
     adam.optimize(100)
     print(adam.theta)
     adam.optimize(100)
