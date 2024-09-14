76d6054738e5a96a70cae75b371cc93b5fb81515
Peter Trubey <peter@trubey.org>
2024-09-14 16:19:53 -0700

    blah

--- run_sphere_vb2.py
+++ run_sphere_vb2.py
@@ -13,7 +13,7 @@ from data import Data_From_Sphere
 from model_spypprg_vb import Chain, Result
 
 source_path = './simulated/sphere2/data_m*_r*_i*.csv'
-out_sql     = './simulated/sphere2/result_240913.sql'
+out_sql     = './simulated/sphere2/result_240914.sql'
 out_table   = 'energy'
 
 def run_model_from_path_wrapper(args):
